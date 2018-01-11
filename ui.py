57e99c94fb5bd47e964ef22dc8a82ff1ecd9c474
Sean Raven <sravendragon0@hotmail.com>
2018-01-10 17:37:18 -0800

    items are sorted by size

--- ui.py
+++ ui.py
@@ -78,7 +78,7 @@ class MainWindow(tk.Frame):
     def insert_node(self, item, tree, parent=''):
         id = self.treeFrame.insert(parent, 'end', text=item.path, values=(item.size,))
         if isinstance(item, dirstat.DirectoryItem):
-            for subitem in item.subitems:
+            for subitem in sorted(item.subitems, key=lambda i: i.size, reverse=True):
                 self.insert_node(subitem, tree, id)
 
 def main():
